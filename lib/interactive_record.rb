715967a55b543cdeca1025f7b8b289d5b524ed2a
Yingqi Chen <chanwk18@hotmail.com>
2019-06-01 20:29:32 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -10,7 +10,7 @@ class InteractiveRecord
 
   def self.column_names
     sql="PRAGMA table_info ('#{table_name}')"
-    
+
     table_info = DB[:conn].execute(sql)
     DB[:conn].execute(sql)
     binding.pry
